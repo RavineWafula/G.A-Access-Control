bc8aef61c6aaa4a12b5e1ef7474f409a2b3f4fb7
RavineWafula <ravinemachani@gmail.com>
2021-09-28 01:15:39 +0300

    updated

--- run.py
+++ run.py
@@ -10,6 +10,6 @@ app = create_app(config_name)
 app.debug = True
 
 if __name__ == '__main__':
-    #app.run()
-    socketio.run(app, host='http://enivar.pythonanywhere.com', port=5000, use_reloader=False, debug=False, threaded=True)
+    app.run()
+    #socketio.run(app, host='127.0.0.1', port=5000, use_reloader=False, debug=False, threaded=True)
     
